6a24cebde55f5053d8687e953e2811ca1c242302
slawomir <sgicala98@gmail.com>
2019-04-27 22:02:15 +0200

    naive for every precision

--- src/naivemath.F90
+++ src/naivemath.F90
@@ -2,11 +2,11 @@ module naivemath
 
     implicit none
 
-    public::naivmull
-    private::naivmull4
+    public :: naivmull
+    private :: naivmull4, naivmull8, naivmull16
 
     interface naivmull
-        procedure naivmull4
+        procedure naivmull4, naivmull8, naivmull16
     end interface
 
 contains
@@ -35,6 +35,52 @@ contains
     end function naivmull4
 
 
+    function naivmull8 (A, B) result(C)
+        implicit none
+
+        real(kind=8), intent(in), dimension(:,:) :: A, B
+        real(kind=8), intent(out), dimension(size(A,1),size(B,2)) :: C
+        integer(kind=4) :: i, j, k
+
+        if (size(A,2) /= size(B,1)) then
+            print*,"Cannot multiply matrices of that shapes"
+        end if
+
+        C = 0
+
+        do i=1, size(A,1)
+            do j=1, size(B,2)
+                do k=1, size(A,2)
+                    C(i,j) = C(i,j) + A(i,k)*B(k,j)
+                end do
+            end do
+        end do
+
+    end function naivmull8
+
+
+    function naivmull16 (A, B) result(C)
+        implicit none
+
+        real(kind=16), intent(in), dimension(:,:) :: A, B
+        real(kind=16), intent(out), dimension(size(A,1),size(B,2)) :: C
+        integer(kind=4) :: i, j, k
+
+        if (size(A,2) /= size(B,1)) then
+            print*,"Cannot multiply matrices of that shapes"
+        end if
+
+        C = 0
+
+        do i=1, size(A,1)
+            do j=1, size(B,2)
+                do k=1, size(A,2)
+                    C(i,j) = C(i,j) + A(i,k)*B(k,j)
+                end do
+            end do
+        end do
+
+    end function naivmull16
 
 
 end module naivemath
