916a7bb9c097822e0ac3b5fe7845d855de41a398
slawomir <sgicala98@gmail.com>
2019-04-29 15:32:28 +0200

    outer loop gives good sizes

--- src/main.F90
+++ src/main.F90
@@ -16,24 +16,25 @@ program main
 
     !call random_seed(put=seed)
 
-    open(unit=1, file="./res/naive4_times", status='REPLACE')
-    open(unit=2, file="./res/naive8_times", status='REPLACE')
-    open(unit=3, file="./res/naive16_times", status='REPLACE')
-    open(unit=4, file="./res/better4_times", status='REPLACE')
-    open(unit=5, file="./res/better8_times", status='REPLACE')
-    open(unit=6, file="./res/better16_times", status='REPLACE')
-    open(unit=7, file="./res/dot4_times", status='REPLACE')
-    open(unit=8, file="./res/dot8_times", status='REPLACE')
-    open(unit=9, file="./res/dot16_times", status='REPLACE')
-    open(unit=10, file="./res/matmul4_times", status='REPLACE')
-    open(unit=11, file="./res/matmul8_times", status='REPLACE')
-    open(unit=12, file="./res/matmul16_times", status='REPLACE')
+    open(unit=1, file="./res/naive4_times.txt", status='REPLACE')
+    open(unit=2, file="./res/naive8_times.txt", status='REPLACE')
+    open(unit=3, file="./res/naive16_times.txt", status='REPLACE')
+    open(unit=4, file="./res/better4_times.txt", status='REPLACE')
+    open(unit=5, file="./res/better8_times.txt", status='REPLACE')
+    open(unit=6, file="./res/better16_times.txt", status='REPLACE')
+    open(unit=7, file="./res/dot4_times.txt", status='REPLACE')
+    open(unit=8, file="./res/dot8_times.txt", status='REPLACE')
+    open(unit=9, file="./res/dot16_times.txt", status='REPLACE')
+    open(unit=10, file="./res/matmul4_times.txt", status='REPLACE')
+    open(unit=11, file="./res/matmul8_times.txt", status='REPLACE')
+    open(unit=12, file="./res/matmul16_times.txt", status='REPLACE')
 
     do i=1,12
         write(i,*) "#n time"
     end do
 
-    do i=10,1280,i
+    i = 10
+    do while(i <= 1280)
 
         allocate(A4(i,i))
         allocate(B4(i,i))
@@ -67,62 +68,62 @@ program main
         call cpu_time(start)
         C4 = naivmull(A4, B4)
         call cpu_time(end)
-        write(1,*) i, " ", start-end
+        write(1,*) i, " ", end-start
 
         call cpu_time(start)
         C8 = naivmull(A8, B8)
         call cpu_time(end)
-        write(2,*) i, " ", start-end
+        write(2,*) i, " ", end-start
 
         call cpu_time(start)
         C16 = naivmull(A16, B16)
         call cpu_time(end)
-        write(3,*) i, " ", start-end
+        write(3,*) i, " ", end-start
 
         call cpu_time(start)
         C4 = bettmull(A4, B4)
         call cpu_time(end)
-        write(4,*) i, " ", start-end
+        write(4,*) i, " ", end-start
 
         call cpu_time(start)
         C8 = bettmull(A8, B8)
         call cpu_time(end)
-        write(5,*) i, " ", start-end
+        write(5,*) i, " ", end-start
 
         call cpu_time(start)
         C16 = bettmull(A16, B16)
         call cpu_time(end)
-        write(6,*) i, " ", start-end
+        write(6,*) i, " ", end-start
 
         call cpu_time(start)
         C4 = dotmull(A4, B4)
         call cpu_time(end)
-        write(7,*) i, " ", start-end
+        write(7,*) i, " ", end-start
 
         call cpu_time(start)
         C8 = dotmull(A8, B8)
         call cpu_time(end)
-        write(8,*) i, " ", start-end
+        write(8,*) i, " ", end-start
 
         call cpu_time(start)
         C16 = dotmull(A16, B16)
         call cpu_time(end)
-        write(9,*) i, " ", start-end
+        write(9,*) i, " ", end-start
 
         call cpu_time(start)
         C4 = matmul(A4, B4)
         call cpu_time(end)
-        write(10,*) i, " ", start-end
+        write(10,*) i, " ", end-start
 
         call cpu_time(start)
         C8 = matmul(A8, B8)
         call cpu_time(end)
-        write(11,*) i, " ", start-end
+        write(11,*) i, " ", end-start
 
         call cpu_time(start)
         C16 = matmul(A16, B16)
         call cpu_time(end)
-        write(12,*) i, " ", start-end
+        write(12,*) i, " ", end-start
 
         if(allocated(A4)) deallocate(A4)
         if(allocated(B4)) deallocate(B4)
@@ -134,6 +135,7 @@ program main
         if(allocated(B16)) deallocate(B16)
         if(allocated(C16)) deallocate(C16)
 
+        i = 2*i
     end do
 
     do i=1,12
